4bc043b491430855f83c00a438220fb8a1379c51
BlackDandi <145791634+BlackDandi@users.noreply.github.com>
2024-01-06 17:13:32 -0600

    cambio del diseño del auth

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from "./logo.svg";
-import "@aws-amplify/ui-react/styles.css";
-import {
-  withAuthenticator,
-  Button,
-  Heading,
-  Image,
-  View,
-  Card,
-} from "@aws-amplify/ui-react";
+import React from 'react';
+import { Amplify } from 'aws-amplify';
 
-function App({ signOut }) {
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
+import awsExports from './aws-exports';
+import { View, Image } from '@aws-amplify/ui-react';
+import { useTheme } from '@aws-amplify/ui-react';
+import Logo from './img/LogoOficial.jpg';
+Amplify.configure(awsExports);
+
+const components = {
+  Header() {
+    const { tokens } = useTheme();
+    return (
+      <View textAlign="center" padding={tokens.space.large}>
+        <Image
+          alt="CIT logo"
+          src= {Logo}
+          width="auto"
+          height="auto" 
+        />
+      </View>
+    );
+  }
+};
+
+export default function App() {
   return (
-    <View className="App">
-      <Card>
-        <Image src={logo} className="App-logo" alt="logo" />
-        <Heading level={1}>We now have Auth!</Heading>
-      </Card>
-      <Button onClick={signOut}>Sign Out</Button>
-    </View>
+    <Authenticator hideSignUp={true} components={components}>
+      {({ signOut, user }) => (
+        <main>
+          <h1>Hello {user.username}</h1>
+          <button onClick={signOut}>Sign out</button>
+        </main>
+      )}
+    </Authenticator>
   );
 }
-
-export default withAuthenticator(App);
